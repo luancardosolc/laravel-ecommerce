f3d0d0c1a87ce64a96a9aa6bdca51d28387cea22
Luan Cardoso <luanleitecardoso@gmail.com>
2021-06-20 15:28:49 -0300

    Test that a product can be deleted

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -11,11 +11,6 @@ class ProductTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_that_a_product_can_be_added()
     {
         $this->withoutExceptionHandling();
@@ -79,6 +74,39 @@ class ProductTest extends TestCase
         ]);
 
         $productUpdatedResponse->assertStatus(200);
-        $this->assertTrue(count(Product::all()) > 0);
+    }
+
+    public function test_that_a_product_can_be_deleted()
+    {
+        $this->withoutExceptionHandling();
+
+        $login = $this->post('/api/register', [
+            'name' => 'User 001',
+            'email' => 'user001@teste.com',
+            'password' => 'User@001',
+            'password_confirmation' => 'User@001',
+        ]);
+
+        $productCreatedResponse = $this->withHeaders(
+            [
+                'Authorization' => 'Bearer '.$login['token'],
+                'Accept' => 'application/json'
+            ]
+        )->post('/api/products', [
+            'name'=> 'IPhone 12 Pro',
+            'slug'=> 'iphone-12-pro',
+            'description'=> 'This is an IPhone 12 Pro',
+            'price'=> '999.99'
+        ]);
+
+        $productDeletedResponse = $this->withHeaders(
+            [
+                'Authorization' => 'Bearer '.$login['token'],
+                'Accept' => 'application/json'
+            ]
+        )->delete('/api/products/'.$productCreatedResponse['id'], []);
+
+        $productDeletedResponse->assertStatus(200);
+        $this->assertTrue(count(Product::all()) === 0);
     }
 }
