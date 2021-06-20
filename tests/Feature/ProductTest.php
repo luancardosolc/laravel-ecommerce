a56d03d049f39eb83562bde9efc89bef391e8c16
Luan Cardoso <luanleitecardoso@gmail.com>
2021-06-20 15:06:10 -0300

    Test that a product can be updated

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -42,4 +42,43 @@ class ProductTest extends TestCase
         $response->assertStatus(201);
         $this->assertTrue(count(Product::all()) > 0);
     }
+
+    public function test_that_a_product_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $login = $this->post('/api/register', [
+            'name' => 'User 001',
+            'email' => 'user001@teste.com',
+            'password' => 'User@001',
+            'password_confirmation' => 'User@001',
+        ]);
+
+        $productCreatedResponse = $this->withHeaders(
+            [
+                'Authorization' => 'Bearer '.$login['token'],
+                'Accept' => 'application/json'
+            ]
+        )->post('/api/products', [
+            'name'=> 'IPhone 12 Pro',
+            'slug'=> 'iphone-12-pro',
+            'description'=> 'This is an IPhone 12 Pro',
+            'price'=> '999.99'
+        ]);
+
+        $productUpdatedResponse = $this->withHeaders(
+            [
+                'Authorization' => 'Bearer '.$login['token'],
+                'Accept' => 'application/json'
+            ]
+        )->put('/api/products/'.$productCreatedResponse['id'], [
+            'name'=> 'IPhone 12 Pro Updated',
+            'slug'=> 'iphone-12-pro-updated',
+            'description'=> 'This is an IPhone 12 Pro Updated',
+            'price'=> '899.99'
+        ]);
+
+        $productUpdatedResponse->assertStatus(200);
+        $this->assertTrue(count(Product::all()) > 0);
+    }
 }
