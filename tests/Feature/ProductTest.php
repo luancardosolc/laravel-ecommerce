1461a2fd6f4eadf03c50d1091a7bd8913ebf7405
Luan Cardoso <luanleitecardoso@gmail.com>
2021-06-20 16:16:47 -0300

    Test that a product can be found

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -108,5 +107,39 @@ class ProductTest extends TestCase
 
         $productDeletedResponse->assertStatus(200);
         $this->assertTrue(count(Product::all()) === 0);
+    }
+
+    public function test_that_a_product_can_be_found()
+    {
+        $this->withoutExceptionHandling();
+
+        $login = $this->post('/api/register', [
+            'name' => 'User 001',
+            'email' => 'user001@teste.com',
+            'password' => 'User@001',
+            'password_confirmation' => 'User@001',
+        ]);
+
+        $productCreatedResponse = $this->withHeaders(
+            [
+                'Authorization' => 'Bearer '.$login['token'],
+                'Accept' => 'application/json'
+            ]
+        )->post('/api/products', [
+            'name'=> 'IPhone 12 Pro',
+            'slug'=> 'iphone-12-pro',
+            'description'=> 'This is an IPhone 12 Pro',
+            'price'=> '999.99'
+        ]);
+
+        $productFindResponse = $this->withHeaders(
+            [
+                'Authorization' => 'Bearer '.$login['token'],
+                'Accept' => 'application/json'
+            ]
+        )->get('/api/products/'.$productCreatedResponse['id'], []);
+
+        $productFindResponse->assertStatus(200);
+        $this->assertTrue($productFindResponse['name'] === 'IPhone 12 Pro');
     }
 }
