f6938a7984572b341f4f06512eaa9d1ea433c54d
Luan Cardoso <luanleitecardoso@gmail.com>
2022-07-07 18:11:18 -0300

    Swagger enabling authentication

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -162,7 +162,7 @@ return [
                 ],
                 */
 
-                /* Open API 3.0 support
+                // Open API 3.0 support
                 'passport' => [ // Unique name of security
                     'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
                     'description' => 'Laravel passport oauth2 security.',
@@ -177,7 +177,6 @@ return [
                         ],
                     ],
                 ],
-                */
             ],
             'security' => [
                 /*
