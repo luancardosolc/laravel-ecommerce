42030a16d46c98c183808fedeb849191caa5c5ec
Luan Cardoso <luanleitecardoso@gmail.com>
2022-07-07 21:14:25 -0300

    Adding authentication to some routes

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::get('/products/search/{name}', [ProductController::class, 'search']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
